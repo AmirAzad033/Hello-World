595ee9adc6e95042b78ecc2db0a61f9ff9dad339
Sharp Java <70138937+AmirAzad033@users.noreply.github.com>
2021-07-10 09:36:58 +0430

    Code cleaning in Java File

--- Java/Java.java
+++ Java/Java.java
@@ -1,5 +1,5 @@
 class Java {
-    static void main (String[] args) {
-        System.out.println ("Hello World!");
+    static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
